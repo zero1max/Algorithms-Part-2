e47d4e211a63e8b3f650c1aa00ae824b3f7c040f
Nigmatjonov Muhammadjon <148071379+zero1max@users.noreply.github.com>
2024-09-29 11:12:04 +0500

    First Commit

---
+++ algorithms/task6.py
@@ -0,0 +1,39 @@
+# Task 1
+
+# a = int(input('a: '))
+
+# if a%2 != 0:
+#     print('Weird')
+# else:
+#     print('Not Weird')
+
+# Task 2
+
+# a = int(input('a: '))
+
+# if a%2 == 0 and 2<= a <= 5:
+#     print('Not Weird')
+# else:
+#     print('Weird')
+
+# Task 3
+
+# a = int(input('a: '))
+
+# if a%2 == 0 and 5<= a <= 20:
+#     print('Weird')
+# else:
+#     print('Not Weird')
+
+# Task 4
+
+# a = int(input('a: '))
+
+# if a%2 == 0 and a>= 20:
+#     print('Not Weird')
+# else:
+#     print('Weird')
+
+#------------------------------OR---------------------------
+
+
